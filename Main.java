d9602a930b0e007ec1c11e020be0b95ad52772a4
VinayLJ <ljvinayofficial@gmail.com>
2024-03-30 22:27:42 +0530

    everything is merged

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("this is a new line of code");
         System.out.println("changes to check for conflicts");
+        System.out.println("this change was done by staging");
     }
 }
